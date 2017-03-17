80faa6c68db91b04dc7ddf2247e6c27cbf7c141e
jrjg <jendrik.jordan@web.de>
2017-03-17 19:16:18 +0100

    fixing bug ($ missing)

--- src/run_benchmark.sh
+++ src/run_benchmark.sh
@@ -32,14 +32,14 @@ cleanLog_path=${cleanLog_path:-/usr/sap/HXE/HDB90/work/cleanLog.log}
 for COUNTER in `seq 1 1`
 do
 	printf "Running benchmark number $COUNTER\n"
-	echo "Benchmark number $COUNTER" >> "cleanLog_path"
+	echo "Benchmark number $COUNTER" >> "$cleanLog_path"
 	hdbsql -i 90 -d SystemDB -u "$username" -p "$password" -I benchAll.sql -T "$log_path" -O /dev/null
 	printf "Cleaning up the log\n"
 	awk '/::GET SERVER PROCESSING TIME.*/,/TIME:\s*([0-9]*)\susec/' "$log_path" >> "$cleanLog_path"
 done
 
 
-echo "Switching to row" >> "cleanLog_path"
+echo "Switching to row" >> "$cleanLog_path"
 printf "Switching to row\n"
 hdbsql -i 90 -d SystemDB -u "$username" -p "$password" -I ./switchToRow.sql
 
@@ -47,12 +47,12 @@ hdbsql -i 90 -d SystemDB -u "$username" -p "$password" -I ./switchToRow.sql
 for COUNTER in `seq 1 1`
 do
 	printf "Running benchmark number $COUNTER\n"
-	echo "Benchmark number $COUNTER" >> "cleanLog_path"
+	echo "Benchmark number $COUNTER" >> "$cleanLog_path"
 	hdbsql -i 90 -d SystemDB -u "$username" -p "$password" -I benchAll.sql -T "$log_path" -O /dev/null
 	printf "Cleaning up the log\n"
 	awk '/::GET SERVER PROCESSING TIME.*/,/TIME:\s*([0-9]*)\susec/' "$log_path" >> "$cleanLog_path"
 done
 
-echo "Switching back to column" >> "cleanLog_path"
+echo "Switching back to column" >> "$cleanLog_path"
 printf "Switching back to column\n"
 hdbsql -i 90 -d SystemDB -u "$username" -p "$password" -I ./switchToCol.sql
