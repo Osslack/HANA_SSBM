e35ba321f342b9b4a230361e55c5e0373b7b02ae
Osslack <simonoswaldvhm@gmail.com>
2017-03-18 20:20:09 +0100

    Fixed inidzes

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -12,13 +12,6 @@ CREATE COLUMN TABLE  customer (
 	PRIMARY KEY ("C_CUSTOMERKEY")
 );
 
-CREATE INDEX idx_c_name ON customer(C_Name);
-CREATE INDEX idx_c_city ON customer(C_City);
-CREATE INDEX idx_c_region ON customer(C_Region);
-CREATE INDEX idx_c_phone ON customer(C_Phone);
-CREATE INDEX idx_c_mktsegment ON customer(C_MktSegment);
-
-
 DROP TABLE  part;
 
 CREATE COLUMN TABLE  part
@@ -35,10 +28,7 @@ CREATE COLUMN TABLE  part
 	PRIMARY KEY (P_PartKey)
 );
 
-CREATE INDEX idx_p_name ON part(P_Name);
-CREATE INDEX idx_p_mfgr ON part(P_MFGR);
-CREATE INDEX idx_p_category ON part(P_Category);
-CREATE INDEX idx_p_brand ON part(P_Brand);
+
 
 DROP TABLE  supplier;
 
@@ -53,9 +43,7 @@ CREATE COLUMN TABLE  supplier (
 	PRIMARY KEY(S_SuppKey)
 );
 
-CREATE INDEX idx_s_city ON supplier(S_City);
-CREATE INDEX idx_s_name ON supplier(S_Name);
-CREATE INDEX idx_s_phone ON supplier(S_Phone);
+
 
 DROP TABLE  dim_date;
 
@@ -104,9 +92,3 @@ CREATE COLUMN TABLE  lineorder
 	LO_ShipMode varchar(10)
 );
 
-CREATE INDEX idx_lo_orderkey_lo_linenumber ON lineorder(LO_OrderKey, LO_LineNumber);
-CREATE INDEX idx_lo_custkey ON lineorder(LO_CustKey);
-CREATE INDEX idx_lo_suppkey ON lineorder(LO_SuppKey);
-CREATE INDEX idx_lo_partkey ON lineorder(LO_PartKey);
-CREATE INDEX idx_lo_orderdatekey ON lineorder(LO_OrderDateKey);
-CREATE INDEX idx_lo_commitdatekey ON lineorder(LO_CommitDateKey);
