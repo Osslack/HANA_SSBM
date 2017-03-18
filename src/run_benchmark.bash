ed6f2c84fba3317c87150a38e8fce84cfa526e51
Arwed Mett <arwed.mett@googlemail.com>
2017-03-18 14:50:57 +0100

    repetitions

--- src/run_benchmark.bash
+++ src/run_benchmark.bash
@@ -2,16 +2,19 @@
 source ./hdbsql.bash
 source ./all_benchmarks.bash
 
+# Settings
+repetitions=250
+
 hdb_ask $1
 
 hdb_init_log
 
 # Run column benchmark
 switch_to "column"
-run_all_benchmarks "/usr/sap/HXE/HDB90/work" 10
+run_all_benchmarks "/usr/sap/HXE/HDB90/work" $repetitions
 
 # Run row benchmark
 switch_to "row"
-run_all_benchmarks "/usr/sap/HXE/HDB90/work" 10
+run_all_benchmarks "/usr/sap/HXE/HDB90/work" $repetitions
 
 hdb_finish_log
