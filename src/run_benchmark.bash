1f82750748065e0463fddb11d1825d322e20f8fa
Osslack <simonoswaldvhm@gmail.com>
2017-03-18 19:54:57 +0100

    WIP

--- src/run_benchmark.bash
+++ src/run_benchmark.bash
@@ -9,12 +9,28 @@ hdb_ask $1
 
 hdb_init_log
 
+#First run without indizes
 # Run column benchmark
 switch_to "column"
 run_all_benchmarks "/usr/sap/HXE/HDB90/work" $repetitions
 
 # Run row benchmark
 switch_to "row"
+run_all_benchmarks "/usr/sap/HXE/HDB90/work" $repetitions
+
+#Run with indizes
+# Run column benchmark
+switch_to "column"
+
+printf "Building indizes"
+hdb_run_file "./sql/addBasicIndizes.sql"
+
+run_all_benchmarks "/usr/sap/HXE/HDB90/work" $repetitions
+
+# Run row benchmark
+switch_to "row"
+printf "Rebuilding lineorder index"
+hdb_run_file "./sql/lineOrderIndex.sql"
 run_all_benchmarks "/usr/sap/HXE/HDB90/work" $repetitions
 
 hdb_finish_log
--- src/sql/switch_to_row.sql
+++ src/sql/switch_to_row.sql
@@ -29,13 +29,6 @@ CREATE ROW TABLE  lineorder
     LO_ShipMode varchar(10)
 );
 
-CREATE INDEX idx_lo_orderkey_lo_linenumber ON lineorder(LO_OrderKey, LO_LineNumber);
-CREATE INDEX idx_lo_custkey ON lineorder(LO_CustKey);
-CREATE INDEX idx_lo_suppkey ON lineorder(LO_SuppKey);
-CREATE INDEX idx_lo_partkey ON lineorder(LO_PartKey);
-CREATE INDEX idx_lo_orderdatekey ON lineorder(LO_OrderDateKey);
-CREATE INDEX idx_lo_commitdatekey ON lineorder(LO_CommitDateKey);
-
 IMPORT FROM CSV FILE '/usr/sap/HXE/HDB90/work/lineorder.tbl' INTO "SYSTEM"."LINEORDER" 
 WITH
 
