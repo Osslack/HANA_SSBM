9cc8e869c14d4bac4c3bac97111ada5aaddfffc8
SAP HANA Database System Administrator <hxeadm@hxehost.localdomain.com>
2017-03-18 13:52:59 +0000

    Fixed switch to col

--- src/sql/switch_to_col.sql
+++ src/sql/switch_to_col.sql
@@ -8,33 +8,3 @@ ALTER TABLE  dim_date COLUMN;
 
 ALTER TABLE lineorder COLUMN;
 
-/*
-DROP TABLE  lineorder;
-
-CREATE COLUMN TABLE  lineorder
-(
-    LO_OrderKey bigint not null,
-    LO_LineNumber tinyint not null,
-    LO_CustKey int not null,
-    LO_PartKey int not null,
-    LO_SuppKey int not null,
-    LO_OrderDateKey int not null,
-    LO_OrderPriority varchar(15),
-    LO_ShipPriority char(1),
-    LO_Quantity tinyint,
-    LO_ExtendedPrice decimal,
-    LO_OrdTotalPrice decimal,
-    LO_Discount decimal,
-    LO_Revenue decimal,
-    LO_SupplyCost decimal,
-    LO_Tax tinyint,
-    LO_CommitDateKey integer not null,
-    LO_ShipMode varchar(10)
-);
-
-IMPORT FROM CSV FILE '/usr/sap/HXE/HDB90/work/lineorder.tbl' INTO "SYSTEM"."LINEORDER" 
-WITH
-
-record delimited by '\n' 
-field delimited by '|';
-*/
