6365dc0b0b8578c7558ed91212494e515dcab29d
osslack <simonoswaldvhm@gmail.com>
2017-03-23 23:58:52 +0100

    Added advanced indizes

---
+++ src/sql/advancedIndizes.sql
@@ -0,0 +1,8 @@
+CREATE INDEX idx_c_nation ON customer(C_Nation);
+
+
+CREATE INDEX idx_s_region ON supplier(S_Region);
+CREATE INDEX idx_s_nation ON supplier(S_Nation);
+
+CREATE INDEX idx_d_year ON dim_date(D_Year);
+#CREATE INDEX idx_d_weeknuminyear_d_year ON dim_date(D_WeekNumInYear, D_Year);
