9c8c2f666dbe4b5d17a9e214d5b6cab20e702da7
osslack <simonoswaldvhm@gmail.com>
2017-03-24 21:06:18 +0100

    stuff

--- src/sql/advancedIndizes.sql
+++ src/sql/advancedIndizes.sql
@@ -5,3 +5,5 @@ CREATE INDEX idx_s_region ON supplier(S_Region);
 CREATE INDEX idx_s_nation ON supplier(S_Nation);
 
 CREATE INDEX idx_d_year ON dim_date(D_Year);
+CREATE INDEX idx_d_yearmonthnum ON dim_date(D_YearMonthNum);
+CREATE INDEX idx_d_weeknuminyear_d_year ON dim_date(D_WeekNumInYear,D_Year);
