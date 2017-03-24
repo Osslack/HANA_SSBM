ee1dc4fbd924960049812ca34dcda5a4746b31df
osslack <simonoswaldvhm@gmail.com>
2017-03-24 19:32:29 +0100

    Added advanced indizes and averages for reference until notebook supports it

--- src/sql/advancedIndizes.sql
+++ src/sql/advancedIndizes.sql
@@ -5,4 +5,3 @@ CREATE INDEX idx_s_region ON supplier(S_Region);
 CREATE INDEX idx_s_nation ON supplier(S_Nation);
 
 CREATE INDEX idx_d_year ON dim_date(D_Year);
-#CREATE INDEX idx_d_weeknuminyear_d_year ON dim_date(D_WeekNumInYear, D_Year);
